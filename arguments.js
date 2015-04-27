570a8375c3cf4c28fabb2471f112b905cd4a9aa5
App Academy Student <appacademy@Apps-Mac-mini-19.local>
2015-04-27 11:10:11 -0400

    Add myBind function to arguments.js

--- arguments.js
+++ arguments.js
@@ -8,3 +8,30 @@ var sum = function () {
 
   return accum;
 };
+
+Function.prototype.myBind = function (obj) {
+  var fn = this;
+  var args = Array.prototype.slice.apply(arguments).slice(1)
+  return function () {
+    return fn.apply(obj, args);
+  };
+};
+
+
+var Cat = function (name) {
+  this.name = name;
+};
+
+Cat.prototype.sayHi = function (owner) {
+  console.log(this.name + " says hi to " + owner);
+};
+
+var Dog = function (name) {
+  this.name = name;
+};
+
+var fluffy = new Dog('fluffy');
+
+var hello = Cat.prototype.sayHi;
+
+hello.myBind(fluffy, "Tom")();
