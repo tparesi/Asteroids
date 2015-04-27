2dbe26ec3fc557de906f701375d3a1e98658c9e7
rhoen+tom <rhoen+tom@appacademy.io>
2015-04-27 19:47:39 -0400

    Create Bullet Class

---
+++ Asteroids/lib/bullet.js
@@ -0,0 +1,16 @@
+;( function() {
+  if (typeof window.Asteroids === 'undefined') {
+    window.Asteroids = {};
+  }
+
+  var Bullet = Asteroids.Bullet = function (args) {
+    args.color = Bullet.COLOR;
+    args.radius = Bullet.RADIUS;
+    Asteroids.MovingObject.call(this, args);
+  }
+  Bullet.COLOR = "#FF0000";
+  Bullet.RADIUS = 2;
+
+  Asteroids.util.inherits(Bullet, Asteroids.MovingObject);
+
+})();
